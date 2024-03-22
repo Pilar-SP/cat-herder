3bf257c98f16082e2f7470aefcff662bfe95f7f7
pilar sanchez <mpilar-93@hotmail.com>
2024-03-22 16:49:19 +0000

    Add route and action for project show page

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "projects", to: "projects#index"
-
+  get "projects/:id", to: "projects#show"
 end
