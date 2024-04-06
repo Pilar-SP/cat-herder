ef4ee17f08aeb9d0ccdaa32f00ee0de353397b4e
pilar sanchez <mpilar-93@hotmail.com>
2024-04-06 18:12:38 +0100

    dry controllers

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,21 +1,18 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
-  def show
-    @task = Task.find(params[:id])
-  end
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_project, only: [:new, :create]
+
+  def show; end
 
   def new
-    @project = Project.find(params[:project_id])
     @task = Task.new
   end
 
-  def edit
-    @task = Task.find(params[:id])
-  end
+  def edit; end
 
   def create
-    @project = Project.find(params[:project_id])
     @task = Task.create(task_params)
     @task.project = @project
     @task.save
@@ -24,7 +21,6 @@ class TasksController < ApplicationController
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     @task.save
 
@@ -32,13 +28,20 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy!
 
     redirect_to project_path(@task.project), status: :see_other
   end
 
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
 
   def task_params
     params.require(:task).permit(:name, :description, :priority)
