e8b90473affc4f833bf6507364bb1325d445c187
pilar sanchez <mpilar-93@hotmail.com>
2024-04-16 16:27:43 +0100

    Add Button component

--- app/javascript/components/ProjectsTable.jsx
+++ app/javascript/components/ProjectsTable.jsx
@@ -5,7 +5,7 @@ import Button from './Button';
 const ProjectsTable = () => {
   return (
     <>
-    <Button />
+    <Button title='Add project'/>
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
       <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
